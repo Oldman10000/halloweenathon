9a0b8a1a480b3a539059492f19a7ba3e84012f8f
oldman10000 <patrickoldman1993@gmail.com>
2021-10-14 15:04:22 +0100

    Add initial js content

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const storyTitle = document.getElementById("storyTitle");
+const allStories = document.getElementById("allStories");
+
+fetch("./stories.json")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    data.forEach((story) => {
+      const allStoriesList = document.createElement("li");
+
+      allStoriesList.innerHTML = story.storyName;
+      allStories.append(allStoriesList);
+    });
+    storyTitle.innerHTML = data[0].storyName;
+  })
+  .catch((err) => console.log(err));
