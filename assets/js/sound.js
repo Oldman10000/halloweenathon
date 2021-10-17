aeb40f3f19c2483730d09331e93078a84016cb44
slavabdev <slav.balykov@gmail.com>
2021-10-17 12:26:31 +0100

    add styling to play/pause; add typing text

--- assets/js/sound.js
+++ assets/js/sound.js
@@ -5,9 +5,9 @@ let text = document.querySelector("#sound-text");
 icon.addEventListener("click", () => {
   if (song.paused) {
     song.play();
-    icon.src = "assets/img/pause-circle-regular.svg";
+    icon.src = "assets/img/pause.png";
   } else {
     song.pause();
-    icon.src = "assets/img/play-circle-regular.svg";
+    icon.src = "assets/img/play.png";
   }
 });
