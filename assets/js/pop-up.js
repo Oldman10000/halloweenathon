6669c9d22376aad8864076ec6c15bfdf7ae5c314
slavabdev <slav.balykov@gmail.com>
2021-10-18 10:53:28 +0100

    added modal

---
+++ assets/js/pop-up.js
@@ -0,0 +1,39 @@
+let modal = document.getElementById("myModal");
+let span = document.getElementsByClassName("close")[0];
+
+window.onload = function () {
+  if (window.innerWidth < 760) {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+};
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth < 760) {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+  true;
+});
